6fd367647e362062325e63a784e6746d2ce22214
JoeC <you@example.com>
2021-08-15 17:50:00 +1000

    init

---
+++ sshbox/5.vm1/variables.tf
@@ -0,0 +1,35 @@
+variable "prefix" {
+  default = "sandbox"
+}
+
+variable "vm" {
+  default = "vm"
+}
+
+variable "instance_count" {
+  default = "2"
+}
+
+variable "size" {
+  default = "Standard_D2as_v4"
+}
+
+variable "location" {
+  default = "australiaeast"
+}
+
+variable "vnet_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "sshdata" {
+  description = "SSH key RSA data."
+}
+
+variable "tag" {
+  default = "UAT Infrastructure"
+}
